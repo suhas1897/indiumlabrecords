118a55da6ef353615762fee5a924f6a497921916
suhas1897 <chalamcharlasuhas911@gmail.com>
2025-03-21 12:35:47 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{
+      textAlign: 'center',
+      padding: '20px',
+      color: '#f0e0c1',
+      fontFamily: "'Poppins', sans-serif",
+      fontSize: '14px',
+      background: '#003d36',
+      width: '100%',
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+    }}>
+      © 2025 Alchemiera Bioelectronics India Pvt Ltd. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
